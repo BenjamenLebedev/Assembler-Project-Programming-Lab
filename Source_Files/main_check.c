bf8182189c22993fe2941e5e972bdccb4be701a9
Benji Laptop <benjamenlebedev@gmail.com>
2024-04-07 17:26:13 +0300

    adding different check

---
+++ Source_Files/main_check.c
@@ -0,0 +1,24 @@
+#include "../Header_Files/ast.h"
+#include "../Header_Files/global_var.h"
+
+void print_ast(frontend_ast *ast);
+   
+int mainss(){
+
+    frontend_ast *ast;
+    char *line = "STR:   mov 2,r2 \n";
+
+    /*“abcdef”*/
+
+    ast = (frontend_ast *) calloc(1, sizeof(frontend_ast));
+    if(ast == NULL){ 
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    *ast = frontend(line);
+
+    /*print_ast(ast);*/
+    
+    frontend_free(ast);
+    return 0;
+}
