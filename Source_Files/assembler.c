e874bca3f84c83e6213affe224e8b3c66611d873
Benji Laptop <benjamenlebedev@gmail.com>
2024-04-14 22:07:04 +0300

    deleting redundant comments

--- Source_Files/assembler.c
+++ Source_Files/assembler.c
@@ -16,14 +16,12 @@ int main(int argc, char *argv[]) {
     void *const *begin;
     void *const *end;
     struct symbol *symbol; 
-/*     int result;*/
 
 
     struct translation_unit *translation_unit = (struct translation_unit*)malloc(sizeof(struct translation_unit));;
     translation_unit->symbols = new_vector(symbol_ctor, symbol_dtor);
     translation_unit->externals = new_vector(extern_ctor, extern_dtor);
     
-
 
     for (i = 1; i < argc; i++){
 
@@ -93,13 +91,7 @@ int main(int argc, char *argv[]) {
                         printf("          -------------------------------  Back error ------------------------------- \n");
                         printf("---------------------------------------------------------------------------------------------------- \n");
                     }
-/*                     print_ob(argv[i]);
-                    if(1){
-                        print_ent(argv[i]);
-                    }
-                    if(1){
-                        print_ext(argv[i]);
-                    } */
+
                 }
                 else{
                     printf("---------------------------------------------------------------------------------------------------- \n");
@@ -115,85 +107,9 @@ int main(int argc, char *argv[]) {
             }
             fclose(amFile);
         }
-        /* free(amFileName); */ 
+
         vector_destroy(&translation_unit->symbols);
         vector_destroy(&translation_unit->externals);
     }
     return 0;
 }
-
-/*     result = firstPass(translation_unit, amFileName, amFile);
-    if (result) {
-        printf("First pass completed with errors. on first file\n\n\n");
-    } else {
-        printf("First pass completed successfully. on first file\n\n\n");
-       
-    }
-
-    printf("********* printing symbols inside translation_unit:\n");
-    VECTOR_FOR_EACH(begin, end, translation_unit->symbols) {
-        if (*begin) {
-            symbol = *begin;
-            printf("********* symbol: %s, type: %d, address: %d\n", symbol->symName, symbol->symType, symbol->address);
-        }
-    }
-
-
-    secondPass(translation_unit, amFileName, amFile);
-    printf("********* printing code image:\n\n\n");
-    print_code_image(translation_unit);
-
-    printf("********* printing data image:\n\n\n");
-    print_data_image(translation_unit);
-
-    printf("********* printing symbols inside translation_unit:\n");
-    VECTOR_FOR_EACH(begin, end, translation_unit->symbols) {
-        if (*begin) {
-            symbol = *begin;
-            printf("********* symbol: %s, type: %d, address: %d\n", symbol->symName, symbol->symType, symbol->address);
-        }
-    }
-
-    strcpy(amFileName , "text");
-    i = make_ob_file(translation_unit, amFileName );
-    if(i == 1){
-        printf("back completed successfully");
-    }
- */
-
-
-/* int main(int argc, char *argv[]){
-
-    FILE *amfile;
-    char *amfileName;
-    int i;
-
-    for (i = 1; i < argc; i++){
-
-        amfileName = preprocessor(argv[i]);
-
-        amfile = fopen(amfileName, "r");
-        if(!amfile){
-            printf("Error: file %s not found\n", amfileName);
-            return 1;
-        }
-        else{
-            if(first_pass(amfile, amfileName)){ 
-                rewind(amfile); 
-                if(second_pass(amfile, amfileName)){ 
-                    print_ob(argv[i]);
-                    if(1){
-                        print_ent(argv[i]);
-                    }
-                    if(1){
-                        print_ext(argv[i]);
-                    }
-                }
-                
-            }
-            fclose(amfile);
-        }
-        free(amfileName); 
-    }
-    return 0;
-} */
