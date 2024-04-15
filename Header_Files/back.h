df616f029a2dbed13b354beb3c1e62cc4bb62367
Benji Laptop <benjamenlebedev@gmail.com>
2024-04-15 17:36:32 +0300

    adding sorting function to sort entry symbols

--- Header_Files/back.h
+++ Header_Files/back.h
@@ -10,4 +10,14 @@ int make_ob_file(const struct translation_unit *translation_unit, char *FileName
 
 int make_extern_file(const struct translation_unit *translation_unit, char *FileName);
 
-int make_entries_file(const struct translation_unit *translation_unit, char *FileName);
+int make_entries_file(struct translation_unit *translation_unit, char *FileName);
+
+/****************************************************************************/
+/**************functions for sorting the symbol(enrty) vector****************/
+/****************************************************************************/
+
+void vector_sort(struct translation_unit *translation_unit);
+
+void vector_sort_merge(struct translation_unit *translation_unit);
+void mergeSort(void **arr, int n);
+void merge(void **arr, void **l, int leftCount, void **r, int rightCount);
