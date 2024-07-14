49a8d97d9a353c78dba2d0ec0261d2f32de95c62
BenjamenLebedev <benjamenlebedev@gmail.com>
2024-07-14 23:51:33 +0300

    small change

--- Source_Files/main_check.c
+++ Source_Files/main_check.c
@@ -7,7 +7,7 @@
 #include "../Header_Files/code_convert.h" 
 #include "../Header_Files/macro_spread.h"
 
-int mainssss(int argc, char *argv[]){
+int mains(int argc, char *argv[]){
 
     FILE *input;
     char filename[100];
